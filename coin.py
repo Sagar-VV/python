91c2ca9b29a8d66ef744d4d34d69196feae2ac52
Sagar-VV <sagar.saini6781@gmail.com>
2022-09-22 15:00:45 +0530

    Adding coin game

---
+++ coin.py
@@ -0,0 +1,15 @@
+class Rupee:
+
+    def __init__(self): 
+        self.value = 1.00
+        self.color = 'silver'
+        self.num_edges = 1
+        self.diameter = 22
+        self.thickness = 3.15
+        self.heads = True
+ 
+
+
+
+
+
