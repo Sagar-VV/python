a9f5b0bb1a7aa1a8a78889142bd0d042c9c9c9bb
Sagar-VV <sagar.saini6781@gmail.com>
2022-09-23 11:16:02 +0530

    Creating Module

---
+++ Module_creation.py
@@ -0,0 +1,19 @@
+# Python program to execute
+# main directly
+# print ("You are outside the main variable")
+
+# if __name__ == "__main__":
+# 	print ("Executed when invoked directly , You are inside the main variable")
+# else:
+# 	print ("Executed when imported")
+
+
+
+
+
+
+
+
+import Script_4_Module
+
+Script_4_Module.my_function()
