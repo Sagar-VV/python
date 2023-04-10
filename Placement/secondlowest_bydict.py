5f2310979d852af58b8124c66156849fbeb6c48a
Sagar-VV <sagar.saini6781@gmail.com>
2023-04-10 23:44:34 +0530

    trying my best

--- Placement/secondlowest_bydict.py
+++ Placement/secondlowest_bydict.py
@@ -1,15 +1,36 @@
-my_dict = {}
-
-for i in range(int(input('Total element in dictonary : '))):
+if __name__ == '__main__':
     
-    name = input('write the name : ')
-    score = input('write the score : ')
-
+    #MyFunc defined as key for sorting using the second element of the sublist
+    def myFunc(x):
+        return x[1]
     
-    my_dict[name] = score
-
-
-arange = (sorted(my_dict.items(), key=lambda kv:
-                 (kv[1], kv[0])))
-
-print (arange)
+    records=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name,score])
+    
+    records.sort(key=myFunc)
+    
+    #Find index till which lowest score is repeated
+    i=1
+    while i<len(records) and records[i][1]==records[i-1][1]:
+        i=i+1
+    
+    
+    #Index i is the first index of second lowest score
+    names=[]
+    
+    #Append name for index i into names []
+    names.append(records[i][0])
+    i=i+1
+    
+    #Append rest of names with same second lowest score
+    while i<len(records) and records[i][1]==records[i-1][1]:
+        names.append(records[i][0])
+        i=i+1
+    
+    #sort list of names
+    names.sort()
+    for name in names:
+        print(name)
